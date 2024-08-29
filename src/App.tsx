2640f2e320f63c6f15a7bfad39aa01bb180b043f
Gregory Potdevin <gregory@appcraft.fr>
2024-08-29 15:25:16 +0200

    fix: add users to public folder

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,9 @@ import "semantic-ui-css/semantic.min.css";
 import { Container, Input, List, Segment } from "semantic-ui-react";
 import "./App.css";
 
+// Semantic-UI Documentation : https://react.semantic-ui.com/
+// Lodash Documentation : https://lodash.com/docs/4.17.15
+
 const App: FC = () => {
   return (
     <div className="App">
